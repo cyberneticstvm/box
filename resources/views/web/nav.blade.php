398a3b73a2de328e8dfd8514dadc8eab4703ba8d
Vijoy Sasidharan <mail@cybernetics.me>
2024-08-08 12:19:54 +0400

    small updates

--- resources/views/web/nav.blade.php
+++ resources/views/web/nav.blade.php
@@ -37,133 +37,38 @@
                     <div class="col-auto">
                         <nav class="main-menu d-none d-lg-inline-block">
                             <ul>
-                                <li class="menu-item-has-children mega-menu-wrap">
-                                    <a href="#">Home</a>
-                                    <ul class="mega-menu">
-                                        <li><a href="#">Multipage</a>
-                                            <ul>
-                                                <li><a href="index.html">Digital Agency</a></li>
-                                                <li><a href="home-web-development.html">Web Development</a></li>
-                                                <li><a href="home-software-company.html">Software Company</a></li>
-                                                <li><a href="home-software-company-2.html">Software Company Style 2</a></li>
-                                                <li><a href="home-startup-company.html">Startup Company</a></li>
-                                                <li><a href="home-it-solution.html">IT Solution</a></li>
-                                                <li><a href="home-web-agency.html">Web Agency</a></li>
-                                                <li><a href="home-startup.html">Home Startup </a></li>
-                                                <li><a href="home-game-solution.html"> Game Solution</a></li>
-                                                <li><a href="home-sass-marketing.html">Sass Marketing</a></li>
-                                                <li><a href="home-sass-landing.html">Sass Landing</a></li>
-
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Multipage</a>
-                                            <ul>
-                                                <li><a href="home-sass-landing-2.html">Sass Landing Style 2</a></li>
-                                                <li><a href="home-app-landing.html">App Landing</a></li>
-                                                <li><a href="home-ai-technology.html">AI Technology</a></li>
-                                                <li><a href="home-cyber-security.html">Cyber Security</a></li>
-                                                <li><a href="home-cyber-security-2.html">Cyber Security Style 2 </a></li>
-                                                <li><a href="home-it-company.html">IT Company</a></li>
-                                                <li><a href="home-digital-marking.html">Digital Marking</a></li>
-                                                <li><a href="home-it-agency.html">IT Agency</a></li>
-                                                <li><a href="home-crm.html">Home CRM</a></li>
-                                                <li><a href="home-it-consulting.html">IT Consulting</a></li>
-                                                <li><a href="home-help-desk.html">Home Help Desk</a></li>
-
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Onepage</a>
-                                            <ul>
-                                                <li><a href="home-digital-agency-op.html">Digital Agency Onepage</a></li>
-                                                <li><a href="home-web-development-op.html">Web Development Onepage</a></li>
-                                                <li><a href="home-software-company-op.html">Software Company Onepage</a></li>
-                                                <li><a href="home-software-company-2-op.html">Software Company Style 2 Onepage</a></li>
-                                                <li><a href="home-startup-company-op.html">Startup Company Onepage</a></li>
-                                                <li><a href="home-it-solution-op.html">IT Solution Onepage</a></li>
-                                                <li><a href="home-web-agency-op.html">Web Agency Onepage</a></li>
-                                                <li><a href="home-startup-op.html">Home Startup Onepage </a></li>
-                                                <li><a href="home-game-solution-op.html"> Game Solution Onepage</a></li>
-                                                <li><a href="home-sass-marketing-op.html">Sass Marketing Onepage</a></li>
-                                                <li><a href="home-sass-landing-op.html">Sass Landing Onepage</a></li>
-
-                                            </ul>
-                                        </li>
-                                        <li><a href="#">Onepage</a>
-                                            <ul>
-                                                <li><a href="home-sass-landing-2-op.html">Sass Landing Style 2 Onepage</a></li>
-                                                <li><a href="home-app-landing-op.html">App Landing Onepage</a></li>
-                                                <li><a href="home-ai-technology-op.html">AI Technology Onepage</a></li>
-                                                <li><a href="home-cyber-security-op.html">Cyber Security Onepage</a></li>
-                                                <li><a href="home-cyber-security-2-op.html">Cyber Security Style 2 Onepage</a></li>
-                                                <li><a href="home-it-company-op.html">IT Company Onepage</a></li>
-                                                <li><a href="home-digital-marking-op.html"> Digital Marking Onepage</a></li>
-                                                <li><a href="home-it-agency-op.html">IT Agency Onepage</a></li>
-                                                <li><a href="home-crm-op.html">Home CRM Onepage</a></li>
-                                                <li><a href="home-it-consulting-op.html">IT Consulting Onepage</a></li>
-                                                <li><a href="home-help-desk-op.html">Home Help Desk</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="{{ route('index') }}">Home</a></li>
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Packers & Movers </a></li>
                                 <li class="menu-item-has-children">
-                                    <a href="#">Services</a>
+                                    <a href="#">Storage</a>
                                     <ul class="sub-menu">
-                                        <li><a href="service.html">Services</a></li>
-                                        <li><a href="service-details.html">Services Details</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="#">Pages</a>
-                                    <ul class="sub-menu">
-                                        <li class="menu-item-has-children">
-                                            <a href="#">Shop</a>
-                                            <ul class="sub-menu">
-                                                <li><a href="shop.html">Shop</a></li>
-                                                <li><a href="shop-details.html">Shop Details</a></li>
-                                                <li><a href="cart.html">Cart Page</a></li>
-                                                <li><a href="checkout.html">Checkout</a></li>
-                                                <li><a href="wishlist.html">Wishlist</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="team.html">Team</a></li>
-                                        <li><a href="team-details.html">Team Details</a></li>
-                                        <li><a href="project.html">Project</a></li>
-                                        <li><a href="project-details.html">Project Details</a></li>
-                                        <li><a href="gallery.html">Gallery</a></li>
-                                        <li><a href="pricing.html">Pricing</a></li>
-                                        <li><a href="faq.html">Faq Page</a></li>
-                                        <li><a href="error.html">Error Page</a></li>
-                                    </ul>
-                                </li>
-                                <li class="menu-item-has-children">
-                                    <a href="#">Blog</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog.html">Blog</a></li>
-                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                        <li><a href="#">Space</a></li>
+                                        <li><a href="#">Boxes</a></li>
+                                        <li><a href="#">Smart Storage Solution</a></li>
                                     </ul>
                                 </li>
                                 <li>
-                                    <a href="contact.html">Contact</a>
+                                    <a href="contact.html">Contact Us</a>
                                 </li>
                             </ul>
                         </nav>
-                        <div class="header-button">
+                        <!--<div class="header-button">
                             <button type="button" class="icon-btn sideMenuToggler d-inline-block d-lg-none">
                                 <i class="far fa-shopping-cart"></i>
                                 <span class="badge">5</span>
                             </button>
                             <button type="button" class="th-menu-toggle d-inline-block d-lg-none"><i class="far fa-bars"></i></button>
-                        </div>
+                        </div>-->
                     </div>
                     <div class="col-auto d-none d-lg-block">
                         <div class="header-button">
-                            <button type="button" class="icon-btn searchBoxToggler"><i class="far fa-search"></i></button>
+                            <!--<button type="button" class="icon-btn searchBoxToggler"><i class="far fa-search"></i></button>
                             <button type="button" class="icon-btn sideMenuToggler">
                                 <i class="far fa-shopping-cart"></i>
                                 <span class="badge">5</span>
-                            </button>
-                            <a href="contact.html" class="th-btn shadow-none">Make Appointment<i class="fas fa-arrow-right ms-2"></i></a>
+                            </button>-->
+                            <a href="#" class="th-btn shadow-none">Get A Quote<i class="fas fa-arrow-right ms-2"></i></a>
                         </div>
                     </div>
                 </div>
