d7f2641ce665f729a7c43aa6405d8772d69abf84
Vijoy Sasidharan <mail@cybernetics.me>
2024-08-26 22:57:21 +0400

    small updates

--- resources/views/web/base.blade.php
+++ resources/views/web/base.blade.php
@@ -214,6 +214,9 @@
     <div class="floating">
         <a href="https://api.whatsapp.com/send?phone=971545958276&text=Hello!%20I%20am%20interested%20in%20your%20service" target="_blank"><img src="{{ asset('/web/assets/img/icon/whatsapp.svg') }}" alt="Move N Store Whatsapp" /></a>
     </div>
+    <div class="floating1">
+        <a href="tel:+971545958276" target="_blank"><img src="{{ asset('/web/assets/img/icon/call-us.svg') }}" alt="Move N Store Call" /></a>
+    </div>
     <!-- Scroll To Top -->
     <div class="scroll-top">
         <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
