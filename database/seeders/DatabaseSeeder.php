04ed5d59aa47e34b4f0dab75d80eb30fd3a71ce4
Cybernetics <mail@cybernetics.me>
2024-09-17 10:11:52 +0530

    small updates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Move N Store',
+            'username' => 'movenstore',
+            'email' => 'info@moveinstore.com',
+            'mobile' => '+971548882525',
+            'password' => bcrypt('0548882525'),
         ]);
     }
 }
