a6ea4d0e7d45a3108059f7e412b442ccc901f485
Cybernetics <mail@cybernetics.me>
2024-10-18 07:51:14 +0530

    small updates

--- resources/views/web/sitemap.blade.php
+++ resources/views/web/sitemap.blade.php
@@ -1,54 +1,101 @@
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+<urlset
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+
     <url>
-        <loc>https://moveinstore.com/</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>1.00</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/about-us</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/about-us</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/services/packers-and-movers-in-dubai</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/services/packers-and-movers-in-dubai</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/services/house-hold-storage-in-dubai</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/services/house-hold-storage-in-dubai</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/services/box-storage-in-dubai</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/services/box-storage-in-dubai</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/services/office-storage-in-dubai</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/services/office-storage-in-dubai</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/blogs</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/blogs</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/contact-us</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/contact-us</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/why-choose-us</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/why-choose-us</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>https://moveinstore.com/privacy-policy</loc>
-        <lastmod>2024-09-18T02:30:47+00:00</lastmod>
+        <loc>https://www.moveinstore.com/privacy-policy</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
         <priority>0.80</priority>
     </url>
+    <url>
+        <loc>https://www.moveinstore.com/blogs/office-storage-in-dubai-safe-and-affordable-solutions</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blogs/furniture-storage-in-dubai-safe-and-secure</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blogs/reliable-moving-with-movers-and-packers-in-dubai</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blog/category/Packing%20and%20Moving</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blog/category/Box%20Storage</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blog/category/Office%20Storage</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blog/category/Household%20Storage</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+    <url>
+        <loc>https://www.moveinstore.com/blog/category/General</loc>
+        <lastmod>2024-10-17T16:03:20+00:00</lastmod>
+        <priority>0.64</priority>
+    </url>
+
+
 </urlset>
